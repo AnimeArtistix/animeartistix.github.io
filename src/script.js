9301fd3c6ecea0e2e4e1cf4370133c61d81b1b41
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2022-08-20 15:56:05 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
+/* The code below is taken from here: https://www.w3schools.com/howto/howto_js_vertical_tabs.asp */
 
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+/* The code above is taken from here: https://www.w3schools.com/howto/howto_js_vertical_tabs.asp */
